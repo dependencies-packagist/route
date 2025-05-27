9f1c4e99a27a781771da4a7950057a5c15e64bdc
jundayw <jundayw@126.com>
2025-05-27 10:12:31 +0800

    RoutingContract::toArray

--- src/Contracts/RoutingContract.php
+++ src/Contracts/RoutingContract.php
@@ -4,4 +4,5 @@ namespace Annotation\Route\Contracts;
 
 interface RoutingContract
 {
+    public function toArray(): array;
 }
--- src/Where.php
+++ src/Where.php
@@ -5,6 +5,7 @@ namespace Annotation\Route;
 use Annotation\Route\Concerns\Arrayable;
 use Annotation\Route\Contracts\WhereContract;
 use Attribute;
+use Override;
 
 #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Where extends Arrayable implements WhereContract
@@ -15,5 +16,13 @@ class Where extends Arrayable implements WhereContract
     )
     {
         //
+    }
+
+    #[Override]
+    public function toArray(): array
+    {
+        return [
+            $this->param => $this->constraint,
+        ];
     }
 }
