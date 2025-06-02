16b5c523f68c78f751b765408c5456a201d7c7e4
jundayw <jundayw@126.com>
2025-06-02 23:36:19 +0800

    Fallback

--- src/Routing/Fallback.php
+++ src/Routing/Fallback.php
@@ -2,11 +2,28 @@
 
 namespace Annotation\Route\Routing;
 
-use Annotation\Route\Concerns\Arrayable;
-use Annotation\Route\Contracts\RoutingContract;
+use Annotation\Route\Contracts\WhereContract;
+use Annotation\Route\Route;
 use Attribute;
+use Override;
 
 #[Attribute(Attribute::TARGET_METHOD)]
-class Fallback extends Arrayable implements RoutingContract
+class Fallback extends Route implements WhereContract
 {
+    public function __construct()
+    {
+        parent::__construct(
+            methods: ['get', 'head'],
+            uri: '{fallbackPlaceholder}',
+            name: 'fallback',
+        );
+    }
+
+    #[Override]
+    public function toArray(): array
+    {
+        return [
+            'fallbackPlaceholder' => '.*',
+        ];
+    }
 }
